2c76145768c1d280a4c94aa26be33ea05b5a4214
Sam Harding <hardinsm@indiana.edu>
2014-09-18 16:46:37 -0400

    Small fix for update check

--- ETTv0.2/Misc/ett_versioncheck.m
+++ ETTv0.2/Misc/ett_versioncheck.m
@@ -4,15 +4,20 @@ toolpath = fileparts(which('ettool.m'));
 filename = dir(fullfile(toolpath, 'ETvers.m'));
 
 onlinevers = urlread('http://129.79.193.101:8000/ETTvers.txt');
-fid = fopen(filename.name);
-fgets(fid);
-fgets(fid);
-myvers = fgets(fid);
-fclose(fid);
-
-if ~strcmp(myvers,onlinevers)
+try
+    fid = fopen(filename.name);
+    fgets(fid);
+    fgets(fid);
+    myvers = fgets(fid);
+    fclose(fid);
+    if ~strcmp(myvers,onlinevers)
+        msgbox('Your ETTool is out of date.  Update before continuing','ETT-Out of Date');
+    end
+catch err
     msgbox('Your ETTool is out of date.  Update before continuing','ETT-Out of Date');
 end
+
+
 
 end
 
