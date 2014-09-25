4c76c652b4089dc794010910d98a4242b27a89ce
Sam Harding <hardinsm@indiana.edu>
2014-09-25 10:39:07 -0400

    Version check refining

--- ETTv0.2/Misc/ett_versioncheck.m
+++ ETTv0.2/Misc/ett_versioncheck.m
@@ -3,8 +3,11 @@ function ett_versioncheck
 toolpath = fileparts(which('ettool.m'));
 filename = dir(fullfile(toolpath, 'ETvers.m'));
 try
-onlinevers = urlread('http://129.79.193.101:8000/ETTvers.txt');
-
+    onlinevers = urlread('http://129.79.193.101:8000/ETTvers.txt');
+catch err
+    msg('Could not connect to Host - Sam''s computer is probably off');
+end
+try
     fid = fopen(filename.name);
     fgets(fid);
     fgets(fid);
