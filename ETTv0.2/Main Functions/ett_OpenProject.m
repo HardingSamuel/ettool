c1881468a2dfac9e93669dd459175b24a41f38c7
Samuel Harding <hardinsm@indiana.edu>
2017-09-18 22:02:40 -0400

    Attempt to resolve the default directory glitch when doing fixation detection. Potential issue when loading files across different computers could result in full path names not existing. When loading .etp files, ettool will now check to ensure that the default directory exists, and prompt the user to update it if it does not.

--- ETTv0.2/Main Functions/ett_OpenProject.m
+++ ETTv0.2/Main Functions/ett_OpenProject.m
@@ -3,12 +3,12 @@ function ETT = ett_OpenProject
 % ett_OpenProject
 % Open an existing ETTool project from a saved location.  This will replace
 % the currently-held project within the tool.
-% 
+%
 % INPUTS:
-% 
+%
 % OUTPUTS:
 % ETT - structure containing the eye-tracking data for the loaded file
-% 
+%
 %% Change Log
 %   [SH] - 04/28/14:   v1 - Creation
 
@@ -17,15 +17,34 @@ function ETT = ett_OpenProject
 ETT = [];
 % have user point to file
 [filename,pathname] = uigetfile(...
-    {'*.etp', 'ETT Project FIles (*.etp)'},...
-    'Select a File to Load');
+  {'*.etp', 'ETT Project FIles (*.etp)'},...
+  'Select a File to Load');
 % only try to load if they actually pointed to a file
 % % should probably also add a check here to make sure the file extension
 % is .etp
 if ischar(filename) && ischar(pathname)
-    load([pathname,filename],'-mat')
+  load([pathname,filename],'-mat')
 else
-    
+  
+end
+
+
+% Check whether the default directory exists, and prompt the user to change
+% it if not. Typically a problem when dealing with the same file on
+% multiple computers.
+
+if exist(ETT.DefaultDirectory) == 0
+  defaultDirFig = questdlg('The Default Directory does not exist -- would you like to update it?',...
+    'ERROR: Default Directory',...
+    'Yes','No','Yes');
+  switch defaultDirFig
+    case 'Yes'
+      [ddPathName] = uigetdir(pwd,...
+        'Select a New Default Directory');
+      ETT.DefaultDirectory = [ddPathName filesep];
+    case 'No'
+      
+  end
 end
 
 end
